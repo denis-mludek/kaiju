8b6670d88cc57ee23d661998ef4a2dd8d738152b
Alexandre Galays <agl@zenexity.com>
2016-09-10 10:25:26 +0200

    Example: BrowserSync now hot-reloads styles

---
+++ example/build/liveReload.js
@@ -0,0 +1,72 @@
+
+const webpack = require('webpack')
+const bs = require('browser-sync').create()
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const extractStyl = ExtractTextPlugin.extract(
+  'style',
+  'css?modules&localIdentName=[name]-[local]-[hash:base64:5]!stylus'
+)
+
+const compiler = webpack({
+  entry: './src/main.ts',
+
+  output: {
+    filename: 'main.js',
+    path: './public/'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.ts(x?)$/, loader: 'ts', exclude: /node_modules/ },
+      { test: /\.styl$/, loader: extractStyl }
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+
+  devtool: 'inline-source-map',
+
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
+})
+
+compiler.watch({}, function(err, stats) {
+  if (err)
+    return console.error('webpack build error')
+
+  var changedModules = stats.compilation.modules.filter(module =>
+    module.built && module.resource
+  )
+  var changedStyleModules = changedModules.filter(module =>
+    module.resource.match(/\.(styl)$/)
+  )
+  var hasOnlyStyleChanges = changedModules.length === changedStyleModules.length
+
+  if (hasOnlyStyleChanges) bs.reload('*.css')
+  else bs.reload()
+})
+
+bs.init({
+  proxy: 'localhost:8000',
+
+  notify: {
+    styles: {
+      top: '10px',
+      right: '10px',
+      margin: '0px',
+      padding: '5px',
+      position: 'fixed',
+      fontSize: '26px',
+      zIndex: '9999',
+      borderRadius: '6px',
+      color: 'white',
+      textAlign: 'center',
+      display: 'block',
+      backgroundColor: '#FF3333'
+    }
+  }
+})
