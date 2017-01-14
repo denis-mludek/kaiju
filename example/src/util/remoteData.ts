c5c8de1be431b67509de897d8d2430ea54dcbcb0
Alexandre Galays <agl@zenexity.com>
2017-01-15 00:07:29 +0100

    Example: Remove now useless cast

--- example/src/util/remoteData.ts
+++ example/src/util/remoteData.ts
@@ -6,8 +6,8 @@ export type RemoteData<D, E> =
   { type: 'success', data: D } |
   { type: 'failure', error: E }
 
-export const NotAsked: RemoteData<any, any> = { type: 'notAsked' as 'notAsked' } // tslint:disable-line:no-any
-export const Loading: RemoteData<any, any> = { type: 'loading' as 'loading' } // tslint:disable-line:no-any
+export const NotAsked: RemoteData<any, any> = { type: 'notAsked' } // tslint:disable-line:no-any
+export const Loading: RemoteData<any, any> = { type: 'loading' } // tslint:disable-line:no-any
 export const Success = <D, E>(data: D): RemoteData<D, E> => ({ type: 'success', data })
 export const Failure = <D, E>(error: E): RemoteData<D, E> => ({ type: 'failure', error })
 
@@ -24,9 +24,9 @@ type Unpacked<D, E> = {
  */
 export function unpack<D, E>(data: RemoteData<D, E>): Unpacked<D, E> {
   switch (data.type) {
-    case 'notAsked':  return { loading: false }
-    case 'loading':   return { loading: true }
-    case 'success':   return { data: data.data, loading: false }
-    case 'failure':   return { error: data.error, loading: false }
+    case 'notAsked': return { loading: false }
+    case 'loading':  return { loading: true }
+    case 'success':  return { data: data.data, loading: false }
+    case 'failure':  return { error: data.error, loading: false }
   }
 }
